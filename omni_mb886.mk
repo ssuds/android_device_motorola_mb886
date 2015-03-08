d127563e5c60ba7610527db8742699a0b03acefa
ssuds <ssuds@users.noreply.github.com>
2015-03-08 03:40:35 -0500

    switch to proper omni makefile naming convention

--- omni_mb886.mk
+++ omni_mb886.mk
@@ -16,7 +16,7 @@ PRODUCT_NAME := omni_mb886
 TARGET_VENDOR_PRODUCT_NAME := MB886_att
 TARGET_VENDOR_DEVICE_NAME := qinara
 
-$(call inherit-product, device/motorola/mb886/omni_mb886.mk)
+$(call inherit-product, device/motorola/mb886/full_mb886.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=motorola/MB886_att/qinara:4.1.1/9.8.0Q-97_MB886_FFW-20/27:user/release-keys \
